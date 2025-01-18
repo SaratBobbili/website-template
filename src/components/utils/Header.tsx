fa5b4a790b0a05109e7f8b24b38d3c6760ef1c51
Dylan Harden <dylanharden33@gmail.com>
2025-01-18 17:23:07 -0600

    remove poppins font

--- src/components/utils/Header.tsx
+++ src/components/utils/Header.tsx
@@ -3,7 +3,7 @@ import HeaderProps from '../../types';
 
 const Header: React.FC<HeaderProps> = (props) => {
   return (
-    <h1 className="text-4xl sm:text-5xl font-poppins py-2 text-center md:text-left">
+    <h1 className="text-4xl sm:text-5xl py-2 text-center md:text-left">
       {props.text}
     </h1>
   );
